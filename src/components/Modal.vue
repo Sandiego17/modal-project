3db880af3e8d56b6cf4378406757540a9e739c30
Henry Tochukwu Uzor <99972646+Sandiego17@users.noreply.github.com>
2023-02-17 14:15:42 +0100

    feat: basic feats of Vue CLI

---
+++ src/components/Modal.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="backdrop">
+        <div class="modal">
+            <h1>Modal Title</h1>
+            <p>modal content</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .modal {
+        background: white;
+        border-radius: 10px;
+        margin: 100px auto;
+        padding: 20px;
+        width: 400px;
+    }
+
+    .backdrop {
+        background: rgba(0, 0, 0, 0.5);
+        height: 100%;
+        position: fixed;
+        top: 0;
+        width: 100%;
+    }
+
+    h1 {
+        color: #03cfb4;
+        border: none;
+        padding: 0;
+    }
+</style>
